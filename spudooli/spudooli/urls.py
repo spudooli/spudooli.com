dc70c7cf8e311f4b5945717647f01125258d40d9
Dave <dave@spudooli.com>
2020-02-19 08:27:52 +1300

    A photo gallery exists

--- spudooli/spudooli/urls.py
+++ spudooli/spudooli/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('photoblog.urls')),
+    path('gallery/', include('gallery.urls')),
 ]
