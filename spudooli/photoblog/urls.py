f4e05cfa65582bdf4f479bc41d14e1b174fc2c1f
Dave <dave@spudooli.com>
2020-02-22 16:24:57 +1300

    Ad postlist page to display all posts

--- spudooli/photoblog/urls.py
+++ spudooli/photoblog/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.PostIndex.as_view(), name='home'),
+    path('postlist', views.PostList.as_view(), name='postlist'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- spudooli/photoblog/views.py
+++ spudooli/photoblog/views.py
@@ -2,12 +2,12 @@ from django.views import generic
 from .models import Post
 
 class PostIndex(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-datetime')
+    queryset = Post.objects.filter(status=1).order_by('-datetime')[0:1]
     template_name = 'index.html'
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-datetime')[0:]
-    template_name = 'list.html'
+    queryset = Post.objects.filter(status=1).order_by('-datetime')
+    template_name = 'postlist.html'
 
 class PostDetail(generic.DetailView):
     model = Post
