1340a4ab964663213768fa913b42767700796f1d
Dave <dave@spudooli.com>
2022-05-21 12:06:14 +1200

    Add next and prevous nav to Recently...

--- spudoolicom/recently.py
+++ spudoolicom/recently.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from spudoolicom import app, db
 from datetime import date
+import datetime
 
 @app.route('/recently', strict_slashes=False, defaults={'recentlydate': None})
 @app.route('/recently/<recentlydate>')
@@ -9,7 +10,31 @@ def recently(recentlydate):
     if recentlydate is None:
         today = date.today()
         recentlydate = today.strftime("%Y-%m-%d")
+    
+    year =  int(recentlydate.split("-")[0])
+    month = int(recentlydate.split("-")[1])
+    day = int(recentlydate.split("-")[2])
+   
+    thedate = date(year, month, day)
+    print(f'Recently {thedate}')
+    prevdate = thedate - datetime.timedelta(days=1)
+    nextdate = thedate + datetime.timedelta(days=1)
+    if thedate == date.today():
+        nextdate = thedate
+    print(f'prev {prevdate} , next {nextdate}')
+    humandate = thedate.strftime("%B %d, %Y")
 
+
+    # prevdate = recentlydate - datetime.timedelta(days=1)
+    # print(prevdate)
+
+    #previous date to recently date
+    cur = db.mysql.connection.cursor()
+    datestart = f'{recentlydate} 00:00:00'
+    dateend = f'{recentlydate} 23:59:59'
+    cur.execute("SELECT event_date, name, type, address, artist, album FROM recently where event_date > %s and event_date < %s order by event_date ASC" , (datestart, dateend) )
+    data = cur.fetchall()
+    cur.close() 
 
     cur = db.mysql.connection.cursor()
     datestart = f'{recentlydate} 00:00:00'
@@ -20,6 +45,6 @@ def recently(recentlydate):
 
     
     
-    return render_template('recently.html', data = data, recentlydate = recentlydate)
+    return render_template('recently.html', data = data, recentlydate = recentlydate, prevdate = prevdate, nextdate = nextdate, humandate = humandate)
 
                           
