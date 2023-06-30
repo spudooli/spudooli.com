8f30628356e79497c320a9228e91e97fbb00490e
Dave <dave@spudooli.com>
2023-07-01 10:29:27 +1200

    Add kitchen streaming webcam

--- spudoolicom/webcam.py
+++ spudoolicom/webcam.py
@@ -4,11 +4,6 @@ import requests
 
 @app.route('/webcam')
 def webcam():
-    response = requests.get("http://192.168.1.194/picture/1/current/")
-
-    file = open("/var/www/spudooli/spudoolicom/static/webcam1.jpg", "wb")
-    file.write(response.content)
-    file.close()
 
 
     return render_template('webcam.html', )
