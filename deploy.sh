03b7c7e9b4aa970d0a224242b866ac2ff2e914ed
Dave <dave@spudooli.com>
2026-03-15 14:31:23 +1300

    Remove redundant cp

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ if [ "$EUID" -ne 0 ]
 fi
 
 
+echo "Running tests..."
+cd /home/dave/Sites/spudooli.com
+sudo -u dave python3 -m pytest || { echo "Tests failed, aborting deploy"; exit 1; }
+
 echo "Copying the app..."
 
 cp /home/dave/Sites/spudooli.com/spudoolicom/*.py /var/www/spudooli/spudoolicom/
@@ -14,8 +18,6 @@ cp /home/dave/Sites/spudooli.com/spudoolicom/*.py /var/www/spudooli/spudoolicom/
 echo "Deploying the static assets..."
 
 cp -r /home/dave/Sites/spudooli.com/spudoolicom/static/* /var/www/spudooli/spudoolicom/static/
-cp -r /home/dave/Sites/spudooli.com/spudoolicom/static/fonts/* /var/www/spudooli/spudoolicom/static/fonts/
-cp -r /home/dave/Sites/spudooli.com/spudoolicom/static/js/* /var/www/spudooli/spudoolicom/static/js/
 
 echo "Deploying the templates..."
 cp -r /home/dave/Sites/spudooli.com/spudoolicom/templates/* /var/www/spudooli/spudoolicom/templates/
@@ -30,5 +32,7 @@ rm -rf /var/www/spudooli/spudoolicom/__pycache__
 
 echo "Restarting Gunicorn..."
 systemctl restart www.spudooli.com.service
+
+sleep 2 && systemctl is-active www.spudooli.com.service || echo "WARNING: service may not be running"
 
 echo "Done"
